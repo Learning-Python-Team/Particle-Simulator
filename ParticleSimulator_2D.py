821a434ed82068eff80e7b587c78edfdb90b60d3
Jp Unknown <jp.unknown47@gmail.com>
2019-08-16 17:00:20 -0400

    added more things; not operational
    Signed-off-by: Jp Unknown <jp.unknown47@gmail.com>

--- ParticleSimulator_2D.py
+++ ParticleSimulator_2D.py
@@ -4,15 +4,17 @@
 
 
 import random
-import time
-import math
+import time as t
+import math as m
 import numpy
 import sys
 import pygame
-from Particle_masses import *
+from ImportantMathForCode import *
+
+start = t.time()
 
 number_of_bodies = 5
-size_of_window = [100, 100]
+size_of_window = 100, 100
 
 Colors = {
 	'White': (255, 255, 255),
@@ -23,7 +25,7 @@ Colors = {
 
 class Particle:
 	def __init__(self, particle_type, position, acceleration, velocity):
-		self.type = particle_type  # This determines what kind of particle it is; one of three is the input 'p', 'n', 'e'
+		self.ptype = particle_type  # This determines what kind of particle it is; one of three is the input 'p', 'n', 'e'
 		self.position = position  # The position is a list [x, y]; position is measured in pixels
 		self.acceleration = acceleration  # Acceleration is a list [x, y] measured in pixels
 		self.velocity = velocity  # Same as Acceleration, but the actual velocity component
@@ -44,13 +46,68 @@ def calculate_forces_between_e_and_n():
 	return None
 
 
-posible_particle_type = ('p', 'e', 'n')
+def gravity(a_pos, a_mass, b_pos, b_mass):
+	x_diff = b_pos[0] - a_pos[0]
+	y_diff = b_pos[1] - a_pos[1]
+	hypotenuse = m.sqrt((x_diff ** 2) + (y_diff ** 2))
+	sin = x_diff / hypotenuse
+	cos = y_diff / hypotenuse
+	
+	f = (G * a_mass * b_mass) / (hypotenuse ** 2)
+	
+	fx = f * sin
+	fy = f * cos
+	
+	return fx, fy
+
+
+possible_particle_type = ('p', 'e', 'n')
 
 particles = []
 for i in range(number_of_bodies):
 	particle_type_int = random.randint(-1, 2)
-	particle_type = str(posible_particle_type[particle_type_int])
+	particle_type = str(possible_particle_type[particle_type_int])
 	pos_x = random.randint(4, (size_of_window[0] - 5))
 	pos_y = random.randint(4, (size_of_window[1] - 5))
 	
 	particles.append(Particle(particle_type, [pos_x, pos_y], [0, 0], [0, 0]))
+
+pygame.init()
+screen = pygame.display.set_mode(size_of_window)
+
+font = pygame.font.SysFont('Arial', 16)
+text = font.render('0', True, Colors['Blue'])
+textRect = text.get_rect()
+simulator_on = 0
+while simulator_on == 0:
+	for event in pygame.event.get():
+		if event.type == pygame.QUIT:
+			simulator_on += 1
+	
+	for particle_a in particles:
+		a_type = particle_a.ptype
+		a_pos = particle_a.position
+		a_accel = particle_a.acceleration
+		a_velo = particle_a.velocity
+		a_mass = particle_a.mass
+		
+		fx_total = 0
+		fy_total = 0
+		
+		for particle_b in particles:
+			b_type = particle_b.ptype
+			b_pos = particle_b.position
+			b_mass = particle_b.mass
+			if b_pos == a_pos:
+				continue
+			fx, fy = 0, 0
+			if a_type == 'p':
+				if b_type == 'e':
+				
+				if b_type == 'n':
+			if a_type == 'e':
+			
+			if a_type == 'n':
+			
+			fx_total += fx
+			fy_total += fy
