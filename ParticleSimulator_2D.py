d916403dcd4f018a5e23359cf6b39be6efe80796
Jp Unknown <jp.unknown47@gmail.com>
2019-08-15 20:12:40 -0400

    Added some changes
    Signed-off-by: Jp Unknown <jp.unknown47@gmail.com>

--- ParticleSimulator_2D.py
+++ ParticleSimulator_2D.py
@@ -8,14 +8,27 @@ import time
 import math
 import sys
 import pygame
+from Particle_masses import *
+
+number_of_bodies = None
+size_of_window = [None, None]
+
+Colors = {
+	'White': (255, 255, 255),
+	'Black': (0, 0, 0),
+	'Blue': (109, 196, 255),
+	}  # (Red, Green, Blue) color values from 0 to 255
 
 
 class Particle:
 	def __init__(self, particle_type, position, acceleration, velocity):
-		self.type = particle_type  # This determines what kind of particle it is; one of three is the input 'P', 'N', 'E'
+		self.type = particle_type  # This determines what kind of particle it is; one of three is the input 'p', 'n', 'e'
 		self.position = position  # The position is a list [x, y]; position is measured in pixels
 		self.acceleration = acceleration  # Acceleration is a list [x, y] measured in pixels
 		self.velocity = velocity  # Same as Acceleration, but the actual velocity component
+		
+		p_type_string = str(particle_type)
+		self.mass = particle_mass[p_type_string]
 
 
 def calculate_forces_between_p_and_n():
---
+++ Particle_masses.py
@@ -0,0 +1,7 @@
+# All masses are in kilograms
+
+particle_mass = {
+	'p': float(1.6726219 * (10 ** (-27))),
+	'e': float(9.10938356 * (10 ** (-31))),
+	'n': float(1.674927471 * (10 ** (-27)))
+	}
